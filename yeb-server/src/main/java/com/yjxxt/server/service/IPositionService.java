f6185c5fb2100a3b953f29cf32ff8630bf3168b2
faharciki <1282146497@qq.com>
2021-09-24 16:53:21 +0800

    yeb职位管理完成

---
+++ yeb-server/src/main/java/com/yjxxt/server/service/IPositionService.java
@@ -0,0 +1,16 @@
+package com.yjxxt.server.service;
+
+import com.yjxxt.server.pojo.Position;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author tk
+ * @since 2021-09-24
+ */
+public interface IPositionService extends IService<Position> {
+
+}
---
+++ yeb-server/src/main/java/com/yjxxt/server/service/impl/PositionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.yjxxt.server.service.impl;
+
+import com.yjxxt.server.pojo.Position;
+import com.yjxxt.server.mapper.PositionMapper;
+import com.yjxxt.server.service.IPositionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author tk
+ * @since 2021-09-24
+ */
+@Service
+public class PositionServiceImpl extends ServiceImpl<PositionMapper, Position> implements IPositionService {
+
+}
